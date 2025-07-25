e9f127db1c1ae6f7764fcc403e711d038d3c83b4
Mark Czirr <77179240+biggerhat@users.noreply.github.com>
2025-07-25 15:57:57 -0400

    Update AppHeader.vue

--- resources/js/components/AppHeader.vue
+++ resources/js/components/AppHeader.vue
@@ -63,10 +63,6 @@ const activeItemStyles = computed(
     () => (url: string) => (isCurrentRoute.value(url) ? 'text-neutral-900 dark:bg-neutral-800 dark:text-neutral-100' : ''),
 );
 
-onMounted(() => {
-    console.log(window.location.pathname);
-})
-
 const mainNavItems: NavItem[] = [
     {
         title: 'Rules',
